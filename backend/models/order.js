f4815b2378561c584a91c6d1a56ff5055e8b093d
piyush singanjude <piyushsinganjude007@gmail.com>
2024-08-19 13:03:55 +0530

    m

---
+++ backend/models/order.js
@@ -0,0 +1,18 @@
+// BackendFolder > models > Order.js
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    order_id: { type: String, required: true },
+    payment_id: { type: String },
+    signature: { type: String },
+    amount: { type: Number, required: true },
+    currency: { type: String, default: 'INR' },
+    receipt: { type: String, required: true },
+    status: { type: String, default: 'created' }, // 'created', 'paid', 'failed'
+    createdAt: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'userModel' }, // Assuming you have a User model
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
