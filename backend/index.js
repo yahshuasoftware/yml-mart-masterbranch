f17ddd81d95553458fea4592d05dd82a876687d7
shrikant <shrikantj0794@gmail.com>
2024-10-02 09:55:54 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const fs = require('fs');
 // app.use('/invoices', express.static(path.join(__dirname, 'invoices')));
 
 app.use(cors({
-    origin: 'http://18.206.206.207:3000',
+    origin: 'http://18.206.206.207',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow cookies and other credentials
