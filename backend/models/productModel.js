f038313ed627681bbc1d17495fe0f150e0af6b0d
piyush singanjude <piyushsinganjude007@gmail.com>
2024-10-06 16:48:55 +0530

    footer

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    productName: String,
-    brandName: String,
-    category: String,
-    subcategory: String,  // New subcategory field
-    productImage: [],
-    description: String,
-    price: Number,
-    sellingPrice: Number,
-    quantity: Number
+    productName: { type: String, required: true },
+    brandName: { type: String, required: true },
+    category: { type: String, required: true },
+    subcategory: { type: String, required: true },  // New subcategory field
+    productImage: [{ type: String }], // Changed to an array of strings
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    sellingPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    soldBy: { type: String, required: true },  // New soldBy field
+    features: { type: String },  // New features field
+    specifications: { type: String },  // New specifications field
 }, {
     timestamps: true
 });
