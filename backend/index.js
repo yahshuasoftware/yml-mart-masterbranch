b3862cf4be0b7ade54e19f4b152ab0dba36a0512
shrikant <shrikantj0794@gmail.com>
2024-10-06 16:50:31 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const fs = require('fs');
 // app.use('/invoices', express.static(path.join(__dirname, 'invoices')));
 
 app.use(cors({
-    origin: 'http://54.208.161.239',
+    origin: 'http://ymlmart.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow cookies and other credentials
