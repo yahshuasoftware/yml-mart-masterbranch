194e18b3c72f54f2443580f4a82044f1b0129b43
piyush singanjude <piyushsinganjude007@gmail.com>
2024-10-10 20:29:52 +0530

    offer

--- frontend/src/components/GroceryCard.js
+++ frontend/src/components/GroceryCard.js
@@ -60,11 +60,12 @@ const GroceryCart = ({ category, heading }) => {
             <div className="flex justify-between items-center">
                 <h2 className='text-2xl font-semibold py-4'>{heading}</h2>
                 <button
-                    className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md transition-transform duration-300 hover:scale-105 hover:bg-blue-600"
-                    onClick={handleViewAll}
-                >
-                    View All
-                </button>
+    className="bg-blue-500 text-white font-semibold py-1 px-2 rounded-md"
+    onClick={handleViewAll}
+>
+    View All
+</button>
+
 
             </div>
 
